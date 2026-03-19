64e857eacfa87aa66d1e85356918fe45da05bd4a
Developer <cergeyyarovikov82@gmail.com>
2026-03-19 17:00:59 +0200

    Red112

--- app/src/main/java/chat/stoat/ndk/NativeLibraries.kt
+++ app/src/main/java/chat/stoat/ndk/NativeLibraries.kt
@@ -1,7 +1,5 @@
 package chat.stoat.ndk
 
-import chat.stoat.BuildConfig
-
 annotation class NativeLibrary(val name: String) {
     companion object {
         const val LIB_NAME_NATIVE_MARKDOWN = "stendal"
@@ -12,8 +10,6 @@ annotation class NativeLibrary(val name: String) {
 object NativeLibraries {
     fun init() {
         System.loadLibrary(NativeLibrary.LIB_NAME_NATIVE_MARKDOWN)
-        System.loadLibrary(NativeLibrary.LIB_NAME_NATIVE_MARKDOWN_V2)
         Stendal.init()
-        FinalMarkdown.init(BuildConfig.DEBUG)
     }
 }
